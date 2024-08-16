d33213667be56f2f2ea24c507d41fa93b1792c1d
opensas <opensas@gmail.com>
2024-08-16 15:33:03 -0300

    add dark mode changed to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,22 @@
 <script>
-	import { ModeWatcher } from 'mode-watcher';
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
 	import '../app.css';
+	import { Button } from '$lib/components/ui/button';
+	import { Moon, Sun } from 'lucide-svelte';
 </script>
 
 <ModeWatcher />
 
-<slot></slot>
+<div class="flex h-screen items-center justify-center">
+	<Button on:click={toggleMode} variant="outline" size="icon" class="absolute right-4 top-4 z-10">
+		<Sun
+			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+		/>
+		<Moon
+			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</Button>
+
+	<slot></slot>
+</div>
