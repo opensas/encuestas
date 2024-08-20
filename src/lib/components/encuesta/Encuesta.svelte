5315f6e192a6216679a399c5585e4eeb6e67ef9d
opensas <opensas@gmail.com>
2024-08-20 11:14:42 -0300

    improve responsive layout

--- src/lib/components/encuesta/Encuesta.svelte
+++ src/lib/components/encuesta/Encuesta.svelte
@@ -15,11 +15,9 @@
 	function onupdate(respuesta: Encuesta['preguntas'][number]['respuesta']) {
 		encuesta.preguntas[current].respuesta = respuesta;
 	}
-
-	$: console.log('!!!', { current, respuesta: encuesta.preguntas[current].respuesta, encuesta });
 </script>
 
-<div class="rounded-[0.5rem] border bg-background shadow-xl">
+<div class="rounded-[0.5rem] bg-background sm:border sm:shadow-xl">
 	<div class="space-y-6 p-6 sm:p-10 md:block">
 		<div class="space-y-1">
 			<h2 class="text-2xl font-bold tracking-tight">{encuesta.titulo}</h2>
