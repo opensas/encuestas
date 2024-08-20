629b3d684abf286ab977182716d8fbeacc2312fa
opensas <opensas@gmail.com>
2024-08-20 10:48:21 -0300

    remove centered content from layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,16 +9,14 @@
 
 <ModeWatcher />
 
-<div class="flex h-screen items-center justify-center">
-	<Button class="absolute right-4 top-4 z-10" size="icon" variant="outline" on:click={toggleMode}>
-		<Sun
-			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-		/>
-		<Moon
-			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-		/>
-		<span class="sr-only">Toggle theme</span>
-	</Button>
+<Button class="absolute right-4 top-4 z-10" size="icon" variant="outline" on:click={toggleMode}>
+	<Sun
+		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+	/>
+	<Moon
+		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
 
-	<slot></slot>
-</div>
+<slot></slot>
