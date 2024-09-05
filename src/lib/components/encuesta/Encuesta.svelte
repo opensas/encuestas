5d0a018f45b0aa5cffadb6196e9a5379d9bf035a
opensas <opensas@gmail.com>
2024-09-05 19:25:19 -0300

    update Encuesta component

--- src/lib/components/encuesta/Encuesta.svelte
+++ src/lib/components/encuesta/Encuesta.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Encuesta } from '$lib/types';
+	import type { Encuesta, Pregunta } from '$lib/types';
 
 	import { Button } from '$lib/components/ui/button';
 	import { Separator } from '$lib/components/ui/separator';
@@ -9,12 +9,84 @@
 	export let encuesta: Encuesta;
 	export let onsave: (encuesta: Encuesta) => void = () => {};
 
-	let current = 0;
-	$: pregunta = encuesta.preguntas[current];
+	// pregunta actualmente siendo respondida
+	let pregunta: Pregunta = encuesta.preguntas[0];
+	$: proxima = updateProxima(pregunta, encuesta);
+
+	$: current = preguntas.length + 1; // numero de pregunta actual, respondidas + 1
+
+	// las preguntas que fueron contestadas
+	let preguntas: Pregunta[] = [];
+
+	function next() {
+		// contesté una pregunta
+		preguntas = [...preguntas, pregunta];
+
+		if (proxima === null) return;
+
+		pregunta = encuesta.preguntas.find((p) => p.id === proxima)!;
+	}
+
+	function updateProxima(pregunta: Pregunta, encuesta: Encuesta) {
+		let proxima = getProxima(pregunta);
+
+		// by default, jump to next pregunta
+		if (proxima === undefined) {
+			const index = encuesta.preguntas.findIndex((p) => p.id === pregunta.id);
+			if (index >= encuesta.preguntas.length - 1) return null;
+			else return encuesta.preguntas[index + 1].id;
+		}
+
+		// null (final) o proxima
+		return proxima;
+	}
+
+	function getProxima(pregunta: Pregunta) {
+		if (!pregunta) return undefined;
+
+		const { tipo, respuesta, proxima } = pregunta;
+
+		if (!respuesta) return proxima;
+
+		// unica o multiple
+		if ('opciones' in pregunta) {
+			// normalize opciones
+			const opciones = pregunta.opciones.map((o) => (typeof o === 'string' ? { titulo: o } : o));
+			for (const opcion of opciones) {
+				let isSelected = false;
+				if (tipo === 'unica' && opcion.titulo === respuesta) isSelected = true;
+				if (tipo === 'multiple' && respuesta.includes(opcion.titulo)) isSelected = true;
+
+				if (isSelected) return opcion.proxima === undefined ? proxima : opcion.proxima;
+			}
+			// check for otros selected, respuesta is not empty, but it doesn't match any option
+			if (respuesta && pregunta.proxima_otros) return pregunta.proxima_otros;
+		}
+
+		// para los otros tipos de pregunta retorno proxima
+		return proxima;
+	}
+
+	function prev() {
+		if (!preguntas.length) return;
+		preguntas = preguntas.slice(0, -1);
+	}
+
+	function save() {
+		// update encuesta con las preguntas efectivamente respondidas
+		next();
+		const saved = { ...encuesta, preguntas };
+		onsave(saved);
+	}
 
 	function onupdate(respuesta: Encuesta['preguntas'][number]['respuesta']) {
-		encuesta.preguntas[current].respuesta = respuesta;
+		pregunta.respuesta = respuesta;
+		encuesta = encuesta;
+		// encuesta.preguntas[current].respuesta = respuesta;
 	}
+
+	$: console.log({ pregunta, respuesta: pregunta.respuesta, proxima });
+	$: console.log({ preguntas, respondidas: preguntas.length });
 </script>
 
 <div class="rounded-[0.5rem] bg-background sm:border sm:shadow-xl">
@@ -29,30 +101,32 @@
 		<Separator class="my-6" />
 
 		<div class="space-y-0.5">
-			<h3 class="text-lg font-medium">{current + 1}. {pregunta.titulo}</h3>
+			<h3 class="text-lg font-medium">{current}. {pregunta.titulo}</h3>
 			{#if pregunta.descripcion}
 				<p class="text-sm text-muted-foreground">{pregunta.descripcion}</p>
 			{/if}
 		</div>
 
-		{#if pregunta.tipo === 'unica'}
-			<Unica {pregunta} {onupdate} />
-		{:else if pregunta.tipo === 'multiple'}
-			<Multiple {pregunta} {onupdate} />
-		{:else if pregunta.tipo === 'puntaje'}
-			<Puntaje {pregunta} {onupdate} />
-		{:else if pregunta.tipo === 'libre'}
-			<Libre {pregunta} {onupdate} />
-		{/if}
+		{#key pregunta.id}
+			{#if pregunta.tipo === 'unica'}
+				<Unica {pregunta} {onupdate} />
+			{:else if pregunta.tipo === 'multiple'}
+				<Multiple {pregunta} {onupdate} />
+			{:else if pregunta.tipo === 'puntaje'}
+				<Puntaje {pregunta} {onupdate} />
+			{:else if pregunta.tipo === 'libre'}
+				<Libre {pregunta} {onupdate} />
+			{/if}
+		{/key}
 
 		<div class="flex justify-between pt-4">
-			<Button class={current <= 0 ? 'invisible' : ''} variant="outline" on:click={() => current--}>
+			<Button class={current <= 1 ? 'invisible' : ''} variant="outline" on:click={prev}>
 				Anterior
 			</Button>
-			{#if current < encuesta.preguntas.length - 1}
-				<Button on:click={() => current++}>Siguiente</Button>
+			{#if proxima !== null}
+				<Button on:click={next}>Siguiente</Button>
 			{:else}
-				<Button on:click={() => onsave(encuesta)}>Finalizar</Button>
+				<Button on:click={save}>Finalizar</Button>
 			{/if}
 		</div>
 	</div>
