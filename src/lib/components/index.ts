3abb0e380b26b34368f01a9c30b7fd39bfcc639e
opensas <opensas@gmail.com>
2025-01-11 03:06:08 -0300

    add combobox component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,15 @@
+import type { Selected } from 'bits-ui';
+
 export { default as Combobox } from './combobox/Combobox.svelte';
 export { default as Input } from './input/Input.svelte';
-export { default as Select, type SelectItem } from './select/Select.svelte';
+export { default as Select } from './select/Select.svelte';
 export { default as Textarea } from './textarea/Textarea.svelte';
+
+// common types (shared by Select and Combobox)
+export type SelectItem = Selected<string>;
+
+export function toItem(value: string | SelectItem): SelectItem {
+	const option = typeof value === 'string' ? { value } : { ...value };
+	option.label = option.label || option.value;
+	return option;
+}
