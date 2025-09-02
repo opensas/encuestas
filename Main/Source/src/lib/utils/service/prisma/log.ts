1f3671183c18f09acb657fe634970c8d02e4d57c
opensas <opensas@gmail.com>
2025-09-02 15:29:54 +0000

    Fix prisma client issue
    see issue at prisma repo: https://github.com/prisma/prisma/discussions/28015
    
    * improve log.ts
    
    * update log to relax PrismaClient type with  PrismaClientWithEvents
    
    * generate client at default location: @prisma/client

--- Main/Source/src/lib/utils/service/prisma/log.ts
+++ Main/Source/src/lib/utils/service/prisma/log.ts
@@ -1,44 +1,35 @@
 import type { SqlLogLevel } from '$lib/server/db';
 
-import type { PrismaClient } from '@prisma/client';
+import type { Prisma, PrismaClient } from '@prisma/client';
 
-export function prismaLog(logLevel: SqlLogLevel = 'NONE') {
+export function prismaLog(logLevel: SqlLogLevel = 'NONE'): Prisma.LogDefinition[] {
 	if (logLevel === 'QUERY') {
 		return [
-			{ emit: 'event', level: 'query' } as const,
-			{ emit: 'stdout', level: 'error' } as const,
+			{ emit: 'event', level: 'query' },
+			{ emit: 'stdout', level: 'error' },
 		];
 	}
 
 	if (logLevel === 'INFO') {
 		return [
-			{ emit: 'event', level: 'query' } as const,
-			{ emit: 'stdout', level: 'error' } as const,
-			{ emit: 'stdout', level: 'info' } as const,
-			{ emit: 'stdout', level: 'warn' } as const,
+			{ emit: 'event', level: 'query' },
+			{ emit: 'stdout', level: 'error' },
+			{ emit: 'stdout', level: 'info' },
+			{ emit: 'stdout', level: 'warn' },
 		];
 	}
 
 	return []; // default and NONE
 }
 
-type QueryEvent = {
-	timestamp: Date;
-	query: string;
-	params: string;
-	duration: number;
-	target: string;
-};
-
-type LogEvent = {
-	timestamp: Date;
-	message: string;
-	target: string;
-};
-
+// Create a type that allows all events regardless of config
+// prisma statically infers the allowed events from the config
+// but since the config is dynamic, it infers never (NONE) as prisma.$on(e: eventType never)
+// we define a type for a widened version of prisma.$on that accept all possible events
 export function prismaLogEvent(prisma: PrismaClient, logLevel: SqlLogLevel = 'NONE') {
 	if (logLevel === 'QUERY' || logLevel === 'INFO') {
-		prisma.$on('query', (e: QueryEvent) => {
+		type $onQuery = (e: 'query', callback: (e: Prisma.QueryEvent) => void) => void;
+		(prisma.$on as $onQuery)('query', (e) => {
 			console.log('🔍 SQL Query:', e.query);
 			console.log('📋 Params:', e.params);
 			console.log('⏱️  Duration:', e.duration, 'ms');
@@ -47,7 +38,9 @@ export function prismaLogEvent(prisma: PrismaClient, logLevel: SqlLogLevel = 'NO
 	}
 
 	if (logLevel === 'INFO') {
-		prisma.$on('info', (e: LogEvent) => console.log('ℹ️ ', e.message));
-		prisma.$on('warn', (e: LogEvent) => console.log('⚠️ ', e.message));
+		type $onLog = (e: 'info' | 'warn' | 'error', callback: (e: Prisma.LogEvent) => void) => void;
+		(prisma.$on as $onLog)('info', (e) => console.log('ℹ️ ', e.message));
+		(prisma.$on as $onLog)('warn', (e) => console.log('⚠️ ', e.message));
+		(prisma.$on as $onLog)('error', (e) => console.log('❌ ', e.message));
 	}
 }
